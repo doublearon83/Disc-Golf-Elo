601068b6d0fc020b831ff9f576819e79f7049499
doublearon83 <ahoward1@fandm.edu>
2022-08-01 12:44:48 -0400

    Update

--- data_prep.r
+++ data_prep.r
@@ -7,17 +7,18 @@ leyy<-nrow(eyy)
 
 logreg_sub1<-subset(logreg,logreg$Ev==eyy$Ev[1] & logreg$year==eyy$year[1])
 logreg_sub1$Etot<-rowSums(cbind(logreg_sub1$E,logreg_sub1$Et),na.rm=T)
-logreg_sub1<-logreg_sub1[,c(5,9)]
+logreg_sub1<-logreg_sub1[,c(5,9,4)]
 logreg_sub1<-logreg_sub1[order(logreg_sub1$Etot,decreasing=T),][1:25,]
 
 logreg_sub2<-subset(logreg,logreg$Ev==eyy$Ev[2] & logreg$year==eyy$year[2])
 logreg_sub2$Etot<-rowSums(cbind(logreg_sub2$E,logreg_sub2$Et),na.rm=T)
-logreg_sub2<-logreg_sub2[,c(5,9)]
+logreg_sub2<-logreg_sub2[,c(5,9,4)]
 logreg_sub2<-logreg_sub2[order(logreg_sub2$Etot,decreasing=T),][1:25,]
 
 for (j in 1:nrow(logreg_sub1)) {
   if (logreg_sub1$name[j] %in% logreg_sub2$name) {
     logreg_sub1$Etot[j] <- logreg_sub2$Etot[match(logreg_sub1$name[j],logreg_sub2$name)]
+    logreg_sub1$Et[j] <- logreg_sub2$Et[match(logreg_sub1$name[j],logreg_sub2$name)]
   }
 }
 
@@ -27,12 +28,13 @@ logreg_rank <- logreg_rank[order(logreg_rank$Etot,decreasing=T),][1:25,]
 for (i in 3:leyy) {
   logreg_sub1<-subset(logreg,logreg$Ev==eyy$Ev[i] & logreg$year==eyy$year[i])
   logreg_sub1$Etot<-rowSums(cbind(logreg_sub1$E,logreg_sub1$Et),na.rm=T)
-  logreg_sub1<-logreg_sub1[,c(5,9)]
+  logreg_sub1<-logreg_sub1[,c(5,9,4)]
   logreg_sub1<-logreg_sub1[order(logreg_sub1$Etot,decreasing=T),][1:25,]
   
   for (j in 1:nrow(logreg_rank)) {
     if (logreg_rank$name[j] %in% logreg_sub1$name) {
       logreg_rank$Etot[j] <- logreg_sub1$Etot[match(logreg_rank$name[j],logreg_sub1$name)]
+      logreg_rank$Et[j] <- logreg_sub1$Et[match(logreg_rank$name[j],logreg_sub1$name)]
     }
   }
   
@@ -44,15 +46,17 @@ for (i in 3:leyy) {
   
 logreg_data_25 <- logreg[logreg$name %in% logreg_rank$name,]
 logreg_data_25$Etot <- rowSums(cbind(logreg_data_25$E,logreg_data_25$Et),na.rm=T)
-logreg_data_25 <- logreg_data_25[,c(5,6,7,8,9)]
+logreg_data_25 <- logreg_data_25[,c(5,6,7,8,9,4)]
 logreg_data_25 <- subset(logreg_data_25,logreg_data_25$year>2016)
 logreg_data_25 <- logreg_data_25[logreg_data_25$Etot!=0,]
 logreg_data_25$Etot <- round(logreg_data_25$Etot,0)
+logreg_data_25$Et <- round(logreg_data_25$Et,0)
 logreg_data_25$date<-as.Date(logreg_data_25$date,format="%d-%m-%Y")
-names(logreg_data_25) <- c("Name","Event","Year","Date","Elo")
+names(logreg_data_25) <- c("Name","Event","Year","Date","Elo","dElo")
 
 logreg_rank$Etot <- round(logreg_rank$Etot,0)
-names(logreg_rank) <- c("Name","Elo")
+logreg_rank$Et <- round(logreg_rank$Et,0)
+names(logreg_rank) <- c("Name","Elo","dElo")
 
 write.csv(logreg_data_25,"EloDashData.csv")
 write.csv(logreg_rank,"EloRank25.csv")
